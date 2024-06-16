ff776ce053441be1ee7dbebecc38747b7f445aa7
MohilMakwana <135405316+MohilMakwana@users.noreply.github.com>
2024-06-16 23:33:50 +0530

    Add files via upload

---
+++ Assignment/Module-11/src/Layout.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Slider from "./Slider"
+import Content from "./Content"
+import Footer from "./Footer"
+import './index.css'
+import './App.css'
+
+export default function Layout() {
+  return (
+    <>
+      <Header></Header>
+      <Slider></Slider>
+      <Content></Content>
+      <Footer></Footer>
+    </>
+  )
+}
