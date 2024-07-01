18810190ed4421963709bde75cdcf3588ec0f739
MohilMakwana <135405316+MohilMakwana@users.noreply.github.com>
2024-07-02 01:41:04 +0530

    Delete Assignment/Module-10 directory

--- Assignment/Module-10/class-app-components/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Company from './components/EmployeeApp'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Company />
-  </React.StrictMode>,
-)
--- Assignment/Module-10/function-app-components/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Substractions from './components/Calc'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Substractions />
-  </React.StrictMode>,
-)
